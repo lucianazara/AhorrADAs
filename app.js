c88a9c379602b14292b4728fa155babf711d0605
Lu Zara <lucininiz@gmail.com>
2023-02-12 18:43:29 -0300

    se agrega evento al boton categorias

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("estas en categorias")
+// seccion categorias
+
+function $(selector){
+    return  document.querySelector(selector);
+}
+
+window.addEventListener("load", () => {
+    $("#btn-categorias").addEventListener("click", (e) => {
+        e.preventDefault()
+        $("#balance").classList.add("is-hidden")
+        $("#nueva-operacion").classList.add("is-hidden")
+    })
+})
