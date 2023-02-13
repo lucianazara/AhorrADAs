ec1e04a103e8aa270af290f44ca949cd93b08e9c
Lu Zara <lucininiz@gmail.com>
2023-02-12 21:19:36 -0300

    boton categoria y balance, eventos agregados

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 // seccion categorias
-
 function $(selector){
     return  document.querySelector(selector);
 }
+// variables
+const botonCategorias = $("#btn-categorias");
+const balance = $("#balance");
+const nuevaOperacion = $("#nueva-operacion");
+const botonBalance = $("#btn-balance");
+const categorias = $("#categorias");
+
 
 window.addEventListener("load", () => {
-    $("#btn-categorias").addEventListener("click", (e) => {
+    //evento boton categorias
+botonCategorias.addEventListener("click", (e) => {
         e.preventDefault()
-        $("#balance").classList.add("is-hidden")
-        $("#nueva-operacion").classList.add("is-hidden")
+        categorias.classList.remove("is-hidden");
+        balance.classList.add("is-hidden")
+        nuevaOperacion.classList.add("is-hidden")
     })
+})
+
+//evento boton balance
+botonBalance.addEventListener("click", (e) => {
+  e.preventDefault()
+  balance.classList.remove("is-hidden");
+  nuevaOperacion.classList.add("is-hidden");
+    if(!categorias.classList.contains("is-hidden")){
+        categorias.classList.add("is-hidden")
+    }
 })
